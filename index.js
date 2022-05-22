55cc4430a806725395c3384d7da79b4b912c99ee
chaotang <chaotang@tencent.com>
2022-05-22 21:57:13 +0800

    1. 生成虚拟 dom

---
+++ index.js
@@ -0,0 +1,9 @@
+import React from './react';
+
+const profile = (
+  <div className="profile">
+    <span className="profile-title">title</span>
+    <h3 className="profile-content">content</h3>
+  </div>
+);
+console.log('profile: ', profile);
